9022004323778db851ddff15b307379a55482ad9
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-28 17:01:51 +0100

    try to add all series

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import flags from './data/flags'
 const APIkey = 'bf3833151ec0112ceeff966557fa120e'
 const APIurlMovie = 'https://api.themoviedb.org/3/search/movie?'
 const APIurlTV = 'https://api.themoviedb.org/3/search/tv?'
+const APIurlImgs = 'https://image.tmdb.org/t/p/w342'
 
 
 function App() {
@@ -21,67 +22,100 @@ function App() {
     setFindMovie(searchMovie)
   }
 
-  function fetchAllData(APIurlMovie, APIurlTV) {
 
-    function fetchDataMovies(url = `${APIurlMovie}api_key=${APIkey}&query=${findMovie}`) {
-      fetch(url)
-        .then(resp => resp.json())
-        .then(data => {
-          console.log(data.results);
-          setAllFindMovies(data.results)
-
-        })
-    }
-    function fetchDataTVs(url = `${APIurlTV}api_key=${APIkey}&query=${findMovie}`) {
-      fetch(url)
-        .then(resp => resp.json())
-        .then(data => {
-          console.log(data.results);
-          setAllFindTV(data.results)
-
-        })
-    }
-    setAllResult(allFindMovies.concat(allFindTV))
+  function fetchDataMovies(urlMovies = `${APIurlMovie}api_key=${APIkey}&query=${findMovie}`, urlTV = `${APIurlTV}api_key=${APIkey}&query=${findMovie}`) {
+    fetch(urlMovies)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data.results);
+        fetch
+        setAllFindMovies(data.results)
+        fetch(urlTV)
+          .then(resp => (resp.json()))
+          .then(data => {
+            console.log(data);
+            setAllFindTV(data.results)
+          })
+        setAllResult(allFindMovies.concat(allFindTV))
+      })
   }
 
-  useEffect(fetchAllData, [findMovie])
   console.log(allResult);
+
+
+  useEffect(fetchDataMovies, [findMovie])
+
 
 
   return (
     <>
+      <GlobalContext.Provider value={{ fetchDataMovies, setAllFindMovies, allFindMovies }}>
+        {/* <AppHeader /> */}
+        <header>
+          <h1>NettoFlixxo</h1>
+          <form onSubmit={handleSearch}>
+            <div className="searchBar"
+              name='searchBox'
+              id='searchBox'
+              placeholder='Search Movies'
+              value={searchMovie}
+              onChange={e => setSearchMovie(e.target.value)}>
+              <input type="search" />
+              <button type='submit' >invia</button>
+            </div>
+          </form>
+        </header>
+        {/* {allFindMovies.map(movie =>
 
-      {/* <AppHeader /> */}
-      <header>
-        <h1>NettoFlixxo</h1>
-        <form onSubmit={handleSearch}>
-          <div className="searchBar"
-            name='searchBox'
-            id='searchBox'
-            placeholder='Search Movies'
-            value={searchMovie}
-            onChange={e => setSearchMovie(e.target.value)}>
-            <input type="search" />
-            <button type='submit' >invia</button>
+          <div key={movie.id} className="card">
+
+            <h3>{movie.title}</h3>
+            <p>originalTitle: {movie.original_title}</p>
+            <div>
+              language: {flags.includes(movie.original_language) ? <img src={`http://localhost:5173/imgs/flags/${movie.original_language}.png`} alt="" /> :
+                <><img src={`http://localhost:5173/imgs/flags/xx.png`} alt="" /><p>{movie.original_language}</p></>
+              }
+            </div>
+
+            <p>Vote: {movie.vote_average}</p>
+
           </div>
-        </form>
-      </header>
-      {allFindMovies.map(movie =>
+        )}
+        {allFindTV.map(movie =>
 
-        <div key={movie.id} className="card">
+          <div key={movie.id} className="card">
 
-          <h3>{movie.title}</h3>
-          <p>originalTitle: {movie.original_title}</p>
-          <div>
-            language: {flags.includes(movie.original_language) ? <img src={`http://localhost:5173/imgs/flags/${movie.original_language}.png`} alt="" /> :
-              <><img src={`http://localhost:5173/imgs/flags/xx.png`} alt="" /><p>{movie.original_language}</p></>
-            }
+            <h3>{movie.title}</h3>
+            <p>originalTitle: {movie.original_title}</p>
+            <div>
+              language: {flags.includes(movie.original_language) ? <img src={`http://localhost:5173/imgs/flags/${movie.original_language}.png`} alt="" /> :
+                <><img src={`http://localhost:5173/imgs/flags/xx.png`} alt="" /><p>{movie.original_language}</p></>
+              }
+            </div>
+
+            <p>Vote: {movie.vote_average}</p>
+
           </div>
+        )} */}
+        {allResult.map(movie =>
 
-          <p>Vote: {movie.vote_average}</p>
+          <div key={movie.id} className="card">
 
-        </div>
-      )}
+            {movie.title ? <h3>{movie.title}</h3> : <h3>{movie.name}</h3>}
+            <img src={`${APIurlImgs}${movie.poster_path}`} alt="" />
+
+            {movie.original_title ? <p>original movie Title: {movie.original_title}</p> : <p>original TV name: {movie.original_name}</p>}
+            <div>
+              language: {flags.includes(movie.original_language) ? <img src={`http://localhost:5173/imgs/flags/${movie.original_language}.png`} alt="" /> :
+                <><img src={`http://localhost:5173/imgs/flags/xx.png`} alt="" /><p>{movie.original_language}</p></>
+              }
+            </div>
+
+            <p>Vote: {movie.vote_average}</p>
+
+          </div>
+        )}
+      </GlobalContext.Provider>
 
     </>
   )
---
+++ src/components/AppCards.jsx
@@ -0,0 +1,25 @@
+// export default function AppCards({[items]}){
+//     return (
+
+
+//         {items.map()}
+//         // {items.map(movie =>
+
+//         //     <div key={movie.id} className="card">
+  
+//         //       <h3>{movie.title}</h3>
+//         //       <p>originalTitle: {movie.original_title}</p>
+//         //       <div>
+//         //         language: {flags.includes(movie.original_language) ? <img src={`http://localhost:5173/imgs/flags/${movie.original_language}.png`} alt="" /> :
+//         //           <><img src={`http://localhost:5173/imgs/flags/xx.png`} alt="" /><p>{movie.original_language}</p></>
+//         //         }
+//         //       </div>
+  
+//         //       <p>Vote: {movie.vote_average}</p>
+  
+//         //     </div>
+//         //   )}
+//     )
+        
+    
+// }
