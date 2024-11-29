1116c08b64a65d43882ce9470fe3e1007e88388c
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-29 12:55:45 +0100

    add components n refactory

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,7 +1,5 @@
 import { useGlobalContext } from "../contexts/GlobalContext";
-import flags from "../data/flags";
 import AppCard from "./AppCard";
-import AppVoteStars from "./AppVoteStars";
 export default function AppMain() {
     const { allResult } = useGlobalContext()
 
@@ -14,32 +12,7 @@ export default function AppMain() {
 
                     {allResult.map(movie =>
                         <AppCard key={movie.id} movie={movie} />
-                        // <div key={movie.id} className="col-3">
 
-                        //     <div key={movie.id} className="card">
-                        //         <div className="cardImg">
-
-                        //             {movie.poste_path ? <div className='noFound'>no Found</div> : <img src={`${API_URL_IMG}${movie.poster_path}`} alt="" />}
-                        //         </div>
-                        //         <div className="cardBody">
-                        //             {movie.title ? <div className='title'> <strong>Title:</strong> {movie.title}</div> : <div className='title'><strong>Title:</strong> {movie.name}</div>}
-
-                        //             {movie.original_title ? <div className='originalN'><strong>Original movie Title: </strong> {movie.original_title}</div> : <div className='originalN'> <strong strong > Original TV name:  </strong > {movie.original_name}</ div>}
-                        //             <div className='overview'><strong>Overviw: </strong>{movie.overview}</div>
-                        //             <div className='language'>
-                        //                 language: {flags.includes(movie.original_language) ? <img src={`http://localhost:5173/imgs/flags/${movie.original_language}.png`} alt="" /> :
-                        //                     <><img src={`http://localhost:5173/imgs/flags/xx.png`} alt="" /><p>{movie.original_language}</p></>
-                        //                 }
-                        //             </div>
-                        //             <div className="stars">
-                        //                 <AppVoteStars vote={movie.vote_average} />
-
-                        //             </div>
-                        //         </div>
-
-
-                        //     </div>
-                        // </div>
                     )}
                 </div>
             </div>
